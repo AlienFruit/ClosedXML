610f82e3133bcca14951133a804c32e07c1b1d4f
a.prockopovich <a.prockopovich@systtech.ru>
2020-05-23 00:35:49 +0200

    feat: add ExternalLinkValue

---
+++ ClosedXML/Excel/Hyperlinks/ExternalLinkValue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClosedXML.Excel
+{
+    public class ExternalLinkValue
+    {
+        public ExternalLinkValue(Uri uri, string displayValue = null, string tooltip = null)
+        {
+            this.DisplayValue = displayValue;
+            this.Uri = uri;
+            this.Tooltip = tooltip;
+        }
+
+        public string DisplayValue { get; }
+
+        public Uri Uri { get; }
+
+        public string Tooltip{ get; }
+
+        public void Valildate()
+        {
+            if (string.IsNullOrEmpty(this.DisplayValue) && this.Uri is null)
+                throw new ArgumentException("Display value and uri cannot be undefined at the same time");
+        }
+
+    }
+}
